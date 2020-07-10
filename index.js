51fe708913f845d8701dcdfe61a0ab8d08a190eb
Ian Mayton <ian.mayton@gmail.com>
2020-07-10 19:06:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 function depthFirstSearch(rootNode, vertices, edges)
 {
   let stack = [];
+  let visited = [];
   stack.push(rootNode)
   
   while(stack.length > 0)
   {
     let currentNode = stack.pop()
-    if(currentNode.discovered == null)
+    if(currentNode.discovered === null)
     {
       currentNode.discovered = true
-      for(let i = 0; i < )
+      visited.push(currentNode)
+      for(let i = 0; i < edges.length; i++)
+      {
+        if(edges[i][0] === currentNode)
+        {
+          stack.push(edges[i][1]);
+        }
+        else if(edges[i][0] == currentNode)
+        {
+          stack.push(edges[i][0])
+        }
+      }
     }
   }
+  return visited
 }
